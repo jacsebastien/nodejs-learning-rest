c5073f938763321ba617f59fa663c129d295ac19
jacsebastien <jac.sebastien@gmail.com>
2021-11-30 14:25:35 +0100

    Creating first get and post routes

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,20 @@
-exports.getPosts = (req, res, next) => {};
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "First Post", content: "This is the first post." }],
+  });
+};
+
+exports.postPost = (req, res, next) => {
+  const { title, content } = req.body;
+
+  // Create post in DB
+  
+  res.status(201).json({
+    message: "Post created successfully.",
+    post: {
+      id: new Date().toISOString(),
+      title,
+      content,
+    },
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -5,5 +5,7 @@ const router = express.Router();
 
 // GET /feed/posts
 router.get("/posts", feedCtrl.getPosts);
+// POST /feed/post
+router.post("/post", feedCtrl.postPost);
 
 module.exports = router;
