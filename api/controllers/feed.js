a592b22795e1c71a2efca23ed35d03afc888b2f3
jacsebastien <jac.sebastien@gmail.com>
2021-12-01 10:43:27 +0100

    Adding server side validation

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator/check");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,16 +18,27 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.postPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "Validation errors",
+      errors: errors.array(),
+    });
+  }
+  
   const { title, content } = req.body;
-
   // Create post in DB
 
   res.status(201).json({
     message: "Post created successfully.",
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title,
       content,
+      creator: {
+        name: "Seb",
+      },
+      createdAt: new Date(),
     },
   });
 };
--- api/routes/feed.js
+++ api/routes/feed.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const feedCtrl = require("../controllers/feed");
+const { body } = require("express-validator/check");
 
 const router = express.Router();
 
 // GET /feed/posts
 router.get("/posts", feedCtrl.getPosts);
 // POST /feed/post
-router.post("/post", feedCtrl.postPost);
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedCtrl.postPost
+);
 
 module.exports = router;
