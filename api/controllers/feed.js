37d4cdef450d3c4379dc48861bcab65fce449f2f
jacsebastien <jac.sebastien@gmail.com>
2021-12-06 16:37:27 +0100

    Fix missing creator userName

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -11,6 +11,7 @@ exports.getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate("creator")
       .skip((currentPage - 1) * itemsPerPage)
       .limit(itemsPerPage);
 
@@ -68,7 +69,7 @@ exports.getPost = async (req, res, next) => {
   const postId = req.params.postId;
 
   try {
-    const post = await Post.findById(postId);
+    const post = await Post.findById(postId).populate("creator");
 
     if (!post) {
       const error = new Error("Post not found.");
