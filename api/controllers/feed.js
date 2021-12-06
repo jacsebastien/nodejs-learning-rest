9b411dcc2ae49a52421c872ce9c3ff4f5eb3549f
jacsebastien <jac.sebastien@gmail.com>
2021-12-06 15:46:06 +0100

    Cleaning post-user relations

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -152,6 +152,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then(() => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then(() => {
       res.status(200).json({ deletedPost: postId });
     })
     .catch((err) => next(err));
