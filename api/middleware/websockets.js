2f15b37d5d893f131d98bbfed51a9789721851ba
jacsebastien <jac.sebastien@gmail.com>
2021-12-06 17:47:09 +0100

    Sharing websocket instance

--- api/middleware/websockets.js
+++ api/middleware/websockets.js
@@ -1,12 +1,27 @@
-exports.initWebsocket = (server) => {
+const socketIo = require("socket.io");
+
+let io;
+
+function initWebsocket(httpServer) {
   const socketCors = {
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
     },
   };
-  const io = require("socket.io")(server, socketCors);
+
+  io = socketIo(httpServer, socketCors);
   io.on("connection", (socket) => {
     console.log("Client connected");
   });
-};
+}
+
+function getIo() {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+
+  return io;
+}
+
+module.exports = { initWebsocket, getIo };
