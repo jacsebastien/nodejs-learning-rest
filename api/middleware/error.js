a8e6a3f59e036ddbc135a1d14e764f5185e6abd6
jacsebastien <jac.sebastien@gmail.com>
2021-12-07 13:31:13 +0100

    Starting new fresh project

--- api/middleware/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = (error, req, res, next) => {
-  console.log(error);
-  const statusCode = error.statusCode || 500;
-  const message = error.message;
-  const errors = error.errors || [];
-  res.status(statusCode).json({ message, errors });
-};
--- frontend/src/util/constants.js
+++
@@ -1,5 +0,0 @@
-export const baseUrl = "http://localhost:8080";
-export const feedBaseUrl = `${baseUrl}/feed`;
-export const authBaseUrl = `${baseUrl}/auth`;
-export const userBaseUrl = `${baseUrl}/user`;
-
