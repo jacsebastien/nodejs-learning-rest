ad4e7d65b02546966a12c4125a6b4fcf656b79d8
jacsebastien <jac.sebastien@gmail.com>
2021-12-07 14:06:21 +0100

    Finishing REST routes

--- routes/todos.ts
+++ routes/todos.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { Todo } from "../models/todo";
 
-const todos: Todo[] = [];
+let todos: Todo[] = [];
 
 const router = Router();
 
@@ -18,7 +18,32 @@ router.post("/todo", (req, res, next) => {
   };
 
   todos.push(newTodo);
-  res.status(200).json({ newTodo });
+  res.status(201).json({ todo: newTodo });
+});
+
+router.put("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+  const text: string = req.body.text;
+
+  const todoIndex = todos.findIndex((t) => t.id === todoId);
+  if (todoIndex !== -1) {
+    todos[todoIndex] = {
+      id: todos[todoIndex].id,
+      text: text,
+    };
+
+    return res.status(200).json({ todos });
+  }
+
+  res.status(404).json({ message: "Todo not found" });
+});
+
+router.delete("/todo/:todoId", (req, res, next) => {
+  const todoId = req.params.todoId;
+
+  todos = todos.filter((t) => t.id !== todoId);
+
+  return res.status(200).json({ id: todoId });
 });
 
 export default router;
